a0a81456260c71e1f21502f5be84a74392071f85
dezhizhang <1541609448@qq.com>
2025-07-22 06:19:35 +0800

    feat: 添加眼泪移动路径

--- Assets/Scripts/Stone.cs
+++ Assets/Scripts/Stone.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// 眼汨移动的路径
+/// </summary>
 public class Stone : MonoBehaviour
 {
-   private GameObject _tearsGo;
+    private GameObject _tearsGo;
+    // 泪点的父对像
 
-   private void Start()
-   {
-      // 加载文件
-      _tearsGo = Resources.Load<GameObject>("Prefabs/tears");
-      
-      
-      
-   }
+    // 所有的泪点
+    private Transform[] roadsTrans;
 
+
+    private void Start()
+    {
+        // 加载文件for
+        _tearsGo = Resources.Load<GameObject>("Prefabs/tears");
+        Transform pointTrans = transform.Find("Points");
+        roadsTrans = new Transform[pointTrans.childCount];
+
+        for (int i = 0; i < roadsTrans.Length; i++)
+        {
+            // 获取所有的路点
+            roadsTrans[i] = pointTrans.GetChild(i);
+        }
+    }
 }
