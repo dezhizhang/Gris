eb933b392f1c78766d3a9186b49c9d9f700774f6
dezhizhang <1541609448@qq.com>
2025-07-23 07:41:51 +0800

    feat: 钢体移动物体

--- Assets/Scripts/Gril.cs
+++ Assets/Scripts/Gril.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class Gril : MonoBehaviour
 {
     // tqq
-    private float _walkFactor;
+    private float _moveFactor;
+
+    // 移动速度
+    private float _moveSpeed;
+    // 刚体组件
+    private Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _moveSpeed = 5.0f;
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
     private void Update()
     {
-        _walkFactor =  Input.GetAxis("Horizontal");
+        _moveFactor = Input.GetAxis("Horizontal");
     }
 
     private void FixedUpdate()
@@ -18,6 +30,10 @@ public class Gril : MonoBehaviour
 
     private void Move()
     {
-        
+        // 移动的方向
+        Vector2 moveDir = Vector2.right * _moveFactor;
+        // 移动的速度
+        Vector2 moveVelocity = moveDir * _moveSpeed;
+        _rb.velocity = moveVelocity;
     }
 }
